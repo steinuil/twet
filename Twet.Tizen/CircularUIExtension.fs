8b8004de9eb272898cab3bc0fbc4cc86048f1d5b
steinuil <steenuil.owl@gmail.com>
2019-06-14 10:36:31 +0200

    started fixing CircleListView

--- Twet.Tizen/CircularUIExtension.fs
+++ Twet.Tizen/CircularUIExtension.fs
@@ -5,8 +5,18 @@ namespace Fabulous.DynamicViews
 module CircularUIExtension =
     open Tizen.Wearable.CircularUI.Forms
 
-    let BarColorAttribKey = AttributeKey<_> "CircleListView_BarColor"
     let ContentAttribKey = AttributeKey<_> "CirclePage_Content"
+
+    let ListBarColorAttribKey = AttributeKey<_> "CircleListView_BarColor"
+    let FooterAttribKey = AttributeKey<_> "CircleListView_Footer"
+    let HasUnevenRowsAttribKey = AttributeKey<_> "CircleListView_HasUnevenRows"
+    let HeaderAttribKey = AttributeKey<_> "CircleListView_Header"
+    let HeaderTemplateAttribKey = AttributeKey<_> "CircleListView_HeaderTemplate"
+    let HorizontalScrollBarVisibilityAttribKey = AttributeKey<_> "CircleListView_HorizontalScrollbarVisibility"
+    let IsGroupingEnabledAttribKey = AttributeKey<_> "CircleListView_IsGroupingEnabled"
+    let ItemsAttribKey = AttributeKey<_> "CircleListView_Items"
+
+    let ScrollBarColorAttribKey = AttributeKey<_> "CircleScrollView_BarColor"
 
 
     type Fabulous.DynamicViews.View with
@@ -14,6 +24,8 @@ module CircularUIExtension =
             (
                 ?content : Fabulous.DynamicViews.ViewElement,
                 // ?onSizeAllocated,
+
+                //#region args
                 ?anchorX,
                 ?anchorY,
                 ?appearing,
@@ -63,11 +75,13 @@ module CircularUIExtension =
                 ?useSafeArea,
                 ?visual,
                 ?widthRequest
+                //#endregion
             ) =
             
             let attribCount = 0 + if Option.isSome content then 1 else 0
 
             let attribs =
+                //#region attribs
                 ViewBuilders.BuildPage(
                     attribCount,
                     ?anchorX=anchorX,
@@ -120,6 +134,7 @@ module CircularUIExtension =
                     ?visual=visual,
                     ?widthRequest=widthRequest
                 )
+                //#endregion
 
             content |> Option.iter (fun v -> attribs.Add(ContentAttribKey, v))
 
@@ -140,6 +155,30 @@ module CircularUIExtension =
             (
                 ?barColor : Xamarin.Forms.Color,
 
+                ?footer : ViewElement,
+                ?hasUnevenRows : bool,
+                ?header : ViewElement,
+                ?headerTemplate : Xamarin.Forms.DataTemplate,
+                ?horizontalScrollBarVisibility : Xamarin.Forms.ScrollBarVisibility,
+                ?isGroupingEnabled : bool,
+                ?isPullToRefreshEnabled : bool,
+                ?isRefreshing : bool,
+                ?itemAppearing : int -> unit,
+                ?itemDisappearing : int -> unit,
+                ?items : seq<ViewElement>,
+                ?itemSelected : int option -> unit,
+                ?itemTapped : int -> unit,
+                ?refreshCommand : unit -> unit,
+                ?refreshControlColor : Xamarin.Forms.Color,
+                ?refreshing : unit -> unit,
+                ?rowHeight : int,
+                ?selectedItem : int option,
+                ?selectionMode : Xamarin.Forms.ListViewSelectionMode,
+                ?separatorColor : Xamarin.Forms.Color,
+                ?separatorVisibility : Xamarin.Forms.SeparatorVisibility,
+                ?verticalScrollBarVisibility : Xamarin.Forms.ScrollBarVisibility,
+
+                //#region args
                 ?anchorX,
                 ?anchorY,
                 ?automationId,
@@ -148,47 +187,26 @@ module CircularUIExtension =
                 ?classId,
                 ?created,
                 ?focused,
-                ?footer,
                 ?gestureRecognizers,
-                ?hasUnevenRows,
-                ?header,
-                ?headerTemplate,
                 ?heightRequest,
                 ?horizontalOptions,
-                ?horizontalScrollBarVisibility,
                 ?inputTransparent,
                 ?isEnabled,
-                ?isGroupingEnabled,
-                ?isPullToRefreshEnabled,
-                ?isRefreshing,
                 ?isTabStop,
                 ?isVisible,
-                ?itemAppearing,
-                ?itemDisappearing,
-                ?items,
-                ?itemSelected,
-                ?itemTapped,
                 ?margin,
                 ?measureInvalidated,
                 ?minimumHeightRequest,
                 ?minimumWidthRequest,
                 ?opacity,
                 ?ref,
-                ?refreshCommand,
-                ?refreshControlColor,
-                ?refreshing,
                 ?resources,
                 ?rotation,
                 ?rotationX,
                 ?rotationY,
-                ?rowHeight,
                 ?scale,
                 ?scaleX,
                 ?scaleY,
-                ?selectedItem,
-                ?selectionMode,
-                ?separatorColor,
-                ?separatorVisibility,
                 ?sizeChanged,
                 ?style,
                 ?styleClass,
@@ -200,15 +218,25 @@ module CircularUIExtension =
                 ?translationY,
                 ?unfocused,
                 ?verticalOptions,
-                ?verticalScrollBarVisibility,
                 ?visual,
                 ?widthRequest
+                //#endregion
             ) =
 
-            let attribCount = 0 + if Option.isSome barColor then 1 else 0
+            let attribCount =
+                0
+                + if Option.isSome barColor then 1 else 0
+                + if Option.isSome footer then 1 else 0
+                + if Option.isSome hasUnevenRows then 1 else 0
+                + if Option.isSome header then 1 else 0
+                + if Option.isSome headerTemplate then 1 else 0
+                + if Option.isSome horizontalScrollBarVisibility then 1 else 0
+                + if Option.isSome isGroupingEnabled then 1 else 0
+                + if Option.isSome items then 1 else 0
 
             let attribs =
-                ViewBuilders.BuildListView(
+                //#region attribs
+                ViewBuilders.BuildItemsView(
                     attribCount,
                     ?anchorX=anchorX,
                     ?anchorY=anchorY,
@@ -218,47 +246,26 @@ module CircularUIExtension =
                     ?classId=classId,
                     ?created=created,
                     ?focused=focused,
-                    ?footer=footer,
                     ?gestureRecognizers=gestureRecognizers,
-                    ?hasUnevenRows=hasUnevenRows,
-                    ?header=header,
-                    ?headerTemplate=headerTemplate,
                     ?heightRequest=heightRequest,
                     ?horizontalOptions=horizontalOptions,
-                    ?horizontalScrollBarVisibility=horizontalScrollBarVisibility,
                     ?inputTransparent=inputTransparent,
                     ?isEnabled=isEnabled,
-                    ?isGroupingEnabled=isGroupingEnabled,
-                    ?isPullToRefreshEnabled=isPullToRefreshEnabled,
-                    ?isRefreshing=isRefreshing,
                     ?isTabStop=isTabStop,
                     ?isVisible=isVisible,
-                    ?itemAppearing=itemAppearing,
-                    ?itemDisappearing=itemDisappearing,
-                    ?items=items,
-                    ?itemSelected=itemSelected,
-                    ?itemTapped=itemTapped,
                     ?margin=margin,
                     ?measureInvalidated=measureInvalidated,
                     ?minimumHeightRequest=minimumHeightRequest,
                     ?minimumWidthRequest=minimumWidthRequest,
                     ?opacity=opacity,
                     ?ref=ref,
-                    ?refreshCommand=refreshCommand,
-                    ?refreshControlColor=refreshControlColor,
-                    ?refreshing=refreshing,
                     ?resources=resources,
                     ?rotation=rotation,
                     ?rotationX=rotationX,
                     ?rotationY=rotationY,
-                    ?rowHeight=rowHeight,
                     ?scale=scale,
                     ?scaleX=scaleX,
                     ?scaleY=scaleY,
-                    ?selectedItem=selectedItem,
-                    ?selectionMode=selectionMode,
-                    ?separatorColor=separatorColor,
-                    ?separatorVisibility=separatorVisibility,
                     ?sizeChanged=sizeChanged,
                     ?style=style,
                     ?styleClass=styleClass,
@@ -270,12 +277,19 @@ module CircularUIExtension =
                     ?translationY=translationY,
                     ?unfocused=unfocused,
                     ?verticalOptions=verticalOptions,
-                    ?verticalScrollBarVisibility=verticalScrollBarVisibility,
                     ?visual=visual,
                     ?widthRequest=widthRequest
                 )
+                //#endregion
 
-            barColor |> Option.iter (fun v -> attribs.Add(BarColorAttribKey, v))
+            barColor |> Option.iter (fun v -> attribs.Add(ListBarColorAttribKey, v))
+            footer |> Option.iter (fun v -> attribs.Add(FooterAttribKey, v))
+            hasUnevenRows |> Option.iter (fun v -> attribs.Add(HasUnevenRowsAttribKey, v))
+            header |> Option.iter (fun v -> attribs.Add(HeaderAttribKey, v))
+            headerTemplate |> Option.iter (fun v -> attribs.Add(HeaderTemplateAttribKey, v))
+            horizontalScrollBarVisibility |> Option.iter (fun v -> attribs.Add(HorizontalScrollBarVisibilityAttribKey, v))
+            isGroupingEnabled |> Option.iter (fun v -> attribs.Add(IsGroupingEnabledAttribKey, v))
+            items |> Option.iter (fun v -> attribs.Add(ItemsAttribKey, v))
 
             let create () = CircleListView()
 
@@ -285,8 +299,17 @@ module CircularUIExtension =
                 (target : CircleListView) =
                 ViewBuilders.UpdateListView(prevOpt, source, target)
                 source.UpdatePrimitive(
-                    prevOpt, target, BarColorAttribKey,
+                    prevOpt, target, ListBarColorAttribKey,
                     (fun target v -> target.BarColor <- v)
+                )
+                source.UpdateElement(
+                    prevOpt, target, FooterAttribKey,
+                    (fun target -> target.Footer),
+                    (fun target v -> target.Footer <- v)
+                )
+                source.UpdatePrimitive(
+                    prevOpt, target, HasUnevenRowsAttribKey,
+                    (fun target v -> target.HasUnevenRows <- v)
                 )
 
             ViewElement.Create(create, update, attribs)
@@ -294,6 +317,7 @@ module CircularUIExtension =
 
         static member inline CircleStackLayout
             (
+                //#region args
                 ?anchorX,
                 ?anchorY,
                 ?automationId,
@@ -340,9 +364,11 @@ module CircularUIExtension =
                 ?verticalOptions,
                 ?visual,
                 ?widthRequest
+                //#endregion
             ) =
 
             let attribs =
+                //#region attribs
                 ViewBuilders.BuildStackLayout(
                     0,
                     ?anchorX=anchorX,
@@ -392,6 +418,7 @@ module CircularUIExtension =
                     ?visual=visual,
                     ?widthRequest=widthRequest
                 )
+                //#endregion
 
             let create () = CircleStackLayout()
 
@@ -400,5 +427,136 @@ module CircularUIExtension =
                 (source : ViewElement)
                 (target : CircleStackLayout) =
                 ViewBuilders.UpdateStackLayout(prevOpt, source, target)
+
+            ViewElement.Create(create, update, attribs)
+
+
+        static member inline CircleScrollView
+            (
+                ?barColor : Xamarin.Forms.Color,
+
+                //#region args
+                ?anchorX,
+                ?anchorY,
+                ?automationId,
+                ?backgroundColor,
+                ?childrenReordered,
+                ?classId,
+                ?content,
+                ?created,
+                ?focused,
+                ?gestureRecognizers,
+                ?heightRequest,
+                ?horizontalOptions,
+                ?horizontalScrollBarVisibility,
+                ?inputTransparent,
+                ?isClippedToBounds,
+                ?isEnabled,
+                ?isTabStop,
+                ?isVisible,
+                ?layoutAreaOverride,
+                ?margin,
+                ?measureInvalidated,
+                ?minimumHeightRequest,
+                ?minimumWidthRequest,
+                ?opacity,
+                ?orientation,
+                ?padding,
+                ?ref,
+                ?resources,
+                ?rotation,
+                ?rotationX,
+                ?rotationY,
+                ?scale,
+                ?scaleX,
+                ?scaleY,
+                ?scrolled,
+                ?scrollTo,
+                ?sizeChanged,
+                ?style,
+                ?styleClass,
+                ?styleId,
+                ?styles,
+                ?styleSheets,
+                ?tabIndex,
+                ?translationX,
+                ?translationY,
+                ?unfocused,
+                ?verticalOptions,
+                ?verticalScrollBarVisibility,
+                ?visual,
+                ?widthRequest
+                //#endregion
+            ) =
+
+            let attribCount = 0 + if Option.isSome barColor then 1 else 0
+
+            let attribs =
+                //#region attribs
+                ViewBuilders.BuildScrollView(
+                    attribCount,
+                    ?anchorX=anchorX,
+                    ?anchorY=anchorY,
+                    ?automationId=automationId,
+                    ?backgroundColor=backgroundColor,
+                    ?childrenReordered=childrenReordered,
+                    ?classId=classId,
+                    ?content=content,
+                    ?created=created,
+                    ?focused=focused,
+                    ?gestureRecognizers=gestureRecognizers,
+                    ?heightRequest=heightRequest,
+                    ?horizontalOptions=horizontalOptions,
+                    ?horizontalScrollBarVisibility=horizontalScrollBarVisibility,
+                    ?inputTransparent=inputTransparent,
+                    ?isClippedToBounds=isClippedToBounds,
+                    ?isEnabled=isEnabled,
+                    ?isTabStop=isTabStop,
+                    ?isVisible=isVisible,
+                    ?layoutAreaOverride=layoutAreaOverride,
+                    ?margin=margin,
+                    ?measureInvalidated=measureInvalidated,
+                    ?minimumHeightRequest=minimumHeightRequest,
+                    ?minimumWidthRequest=minimumWidthRequest,
+                    ?opacity=opacity,
+                    ?orientation=orientation,
+                    ?padding=padding,
+                    ?ref=ref,
+                    ?resources=resources,
+                    ?rotation=rotation,
+                    ?rotationX=rotationX,
+                    ?rotationY=rotationY,
+                    ?scale=scale,
+                    ?scaleX=scaleX,
+                    ?scaleY=scaleY,
+                    ?scrolled=scrolled,
+                    ?scrollTo=scrollTo,
+                    ?sizeChanged=sizeChanged,
+                    ?style=style,
+                    ?styleClass=styleClass,
+                    ?styleId=styleId,
+                    ?styles=styles,
+                    ?styleSheets=styleSheets,
+                    ?tabIndex=tabIndex,
+                    ?translationX=translationX,
+                    ?translationY=translationY,
+                    ?unfocused=unfocused,
+                    ?verticalOptions=verticalOptions,
+                    ?verticalScrollBarVisibility=verticalScrollBarVisibility,
+                    ?visual=visual,
+                    ?widthRequest=widthRequest
+                )
+                //#endregion
+
+            barColor |> Option.iter (fun v -> attribs.Add(ScrollBarColorAttribKey, v))
+
+            let create () = CircleScrollView()
+
+            let update prevOpt source (target : CircleScrollView) =
+                ViewBuilders.UpdateScrollView(prevOpt, source, target)
+                source.UpdatePrimitive(
+                    prevOpt, target, ScrollBarColorAttribKey,
+                    (fun target v -> target.BarColor <- v)
+                )
 
             ViewElement.Create(create, update, attribs)
